d3bb6675b1776ea042e6626837237d2b84d5a754
Jonathan Stendahl <jonathan.stendahl@starrepublic.com>
2019-03-12 11:01:38 +0100

    Adding some bad code..

---
+++ SonarCloudDemo/Infrastructure/BadClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SonarCloudDemo.Infrastructure
+{
+    public class BadClass
+    {
+        public string SomeMethod()
+        {
+            var methodResult = ShouldBePrivate();
+            return methodResult;
+        }
+
+        public string ShouldBePrivate()
+        {
+            var list = new List<object>();
+
+            var url = "https://www.google.se";
+
+            var client = new HttpClient();
+
+            var result = client.GetAsync(url).Result;
+
+            return result.Content.ReadAsStringAsync().Result;
+
+        }
+
+    }
+}
